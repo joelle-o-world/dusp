f9442994ed334b717bef78f67235cde905110953
Joel Plowright <joelplow@gmail.com>
2019-02-27 12:02:05 +0000

    add index, increment version

---
+++ src/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // useful functions
+  unDusp: require('./unDusp'),
+  dusp: require('./dusp'),
+  renderChannelData: require("./renderChannelData"),
+  channelDataToAudioBuffer: require('./webaudioapi/channelDataToAudioBuffer'),
+  connectToWAA: require("./webaudioapi/connectToWAA"),
+
+  quick: require('./quick'),
+
+  // basic elements
+  Unit: require("./Unit"),
+  Patch: require("./Patch"),
+  Circuit: require("./Circuit"),
+
+  components: require('./components'),
+  patches: require('./patches'),
+}
