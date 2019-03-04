91a99fcf58fab9ea588fb46dd83ff4a1af0a5cd5
Joel Plowright <joelplow@gmail.com>
2019-03-04 17:45:51 +0000

    expressions may begin with comments or whitespace

--- demos/dusp-player-demo.bundle.js
+++ demos/dusp-player-demo.bundle.js
@@ -6577,7 +6577,7 @@ module.exports = getDotProperty
 function getExpression(str, i0) {
   i0 = i0 || 0
 
-  var i1 = i0
+  var i1 = skipCommentsAndWhitespace(str, i0)
   /*if(str[i0] == "(") {
     var bracketed = true
     i1++
